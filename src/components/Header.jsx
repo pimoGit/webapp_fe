3c4b5145da46b8121bbdc5b14f4fcf5ece6beb5d
Simone <info@simoneicardi.com>
2025-03-11 11:35:19 +0100

    aggiunta moduli e crea header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+// importiamo parte LInk del modulo react-router
+import { Link } from "react-router-dom"
+
+const Header = () => {
+
+    return (
+        <nav className="navbar bg-body-tertiary mb-4">
+            <div className="container-fluid justify-content-start">
+                <Link className="navbar-brand" to="/">Bool Books</Link>
+            </div>
+        </nav>
+    )
+
+}
+
+
+export default Header
