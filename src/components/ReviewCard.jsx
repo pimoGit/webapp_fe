1bda7ed0da4a7f2c32893ae0edff61bf3a1d2f0d
Simone <info@simoneicardi.com>
2025-03-12 11:54:43 +0100

    dettaglio e recensioni complete

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,16 +1,20 @@
-const ReviewCard = () => {
+const ReviewCard = ({ reviewProp }) => {
+
+    // destruttutiamo la prop
+    const { name, text, vote } = reviewProp
+
     return (
         <div className="card mb-4">
             <div className="card-body">
                 <p className="card-text">
-                    testo della recensione
+                    {text}
                 </p>
                 <strong>
-                    Vote: voto recensione
+                    Vote: {vote}
                 </strong>
                 <address>
                     <i>
-                        By nome del recensore
+                        By {name}
                     </i>
                 </address>
             </div>
--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,27 +1,66 @@
+// uso axios
+import axios from 'axios'
+
+// uso di state e effect
+import { useState, useEffect } from "react"
+
 // importiamo parte LInk del modulo react-router
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 // import del componente di listato
 import ReviewCard from './../components/ReviewCard';
 
 
 const BookPage = () => {
+
+    // recuperiamo l'id del libro richiesto
+    const { id } = useParams();
+
+    // settiamo lo stato del componente
+    const [book, setBook] = useState({});
+
+    // funzione di chiamata all'API per il libro richiesto
+    const fectBook = () => {
+        axios.get("http://localhost:3000/api/books/" + id)
+            .then(
+                res => {
+                    // console.log(res.data);
+                    setBook(res.data)
+                }
+            )
+            .catch(err => console.log(err)
+            )
+    }
+
+
+    // chiamata all'API al montaggio del componente
+    useEffect(fectBook, [])
+
+    // funzione di rendering delle reviews
+    const renderReviews = () => {
+        return book.reviews?.map(
+            review => <ReviewCard key={review.id} reviewProp={review} />
+        )
+        // console.log(book.reviews);
+
+    }
+
     return (
         <>
             <header id="book" className="border-bottom border-1 mb-3">
                 <div className="d-flex mb-3">
                     <img className="book-img"
-                        src="http://localhost:3000/img/books/great-gatsby.jpg"
-                        alt="descrizione img" />
+                        src={book.image}
+                        alt={book.title} />
                     <div>
-                        <h1>Titolo libro</h1>
+                        <h1>{book.title}</h1>
                         <h3 className="text-muted">
                             <i>
-                                By Nome autore
+                                By {book.author}
                             </i>
                         </h3>
                         <p>
-                            Abscract lorem ipsm dolor sit amet...
+                            {book.abstract}
                         </p>
                     </div>
                 </div>
@@ -33,9 +72,7 @@ const BookPage = () => {
                     <h4>Our community reviews</h4>
                 </header>
                 {/* Reviews qui */}
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
+                {renderReviews()}
             </section>
 
             <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
